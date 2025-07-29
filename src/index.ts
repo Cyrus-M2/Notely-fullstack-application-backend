53d3046075aec86ef1e92a3d1e75fa6f156c6c09
Cyrus-M2 <devsben345@gmail>
2025-07-29 14:30:43 +0300

    setting up cors

--- src/index.ts
+++ src/index.ts
@@ -39,11 +39,14 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    process.env.FRONTEND_URL || 'http://localhost:5173'
-  ],
+  origin: process.env.NODE_ENV === 'production' 
+    ? [ 'https://notely-fullstack-application-fronte.vercel.app/' ] 
+    : 'http://localhost:5173',    
+    // 'http://localhost:5173',
+    // 'http://127.0.0.1:5173',
+    // 'https://notely-fullstack-application-fronte.vercel.app/',
+    // process.env.FRONTEND_URL || 'http://localhost:5173'
+  
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
